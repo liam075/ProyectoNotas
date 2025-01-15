2de687c9127d7dd1d5e022aa45d1761a0963f10e
MiguelRodac <rodacmig19@gmail.com>
2025-01-15 12:02:25 -0400

    Crear Tareas

--- src/app/Pages/tasks/tasks.component.ts
+++ src/app/Pages/tasks/tasks.component.ts
@@ -20,9 +20,15 @@ export class TasksComponent implements OnInit,AfterViewInit {
   isModalVisible = false;
   id : number;
   lista_tareas :any = [];
+  lista_hacer : any = [];
+  lista_progreso : any = [];
+  lista_terminado : any = [];
+  category_id : number;
+
 
   constructor(private fb: FormBuilder,private router: Router , private activatedroute :ActivatedRoute) {
     this.formulario_lista = this.fb.group({
+      category_id : [''],
       title: ['', [Validators.required]],
       subtitle: ['', [Validators.required]],
     });
@@ -47,8 +53,54 @@ export class TasksComponent implements OnInit,AfterViewInit {
     });
     let params  = this.activatedroute.snapshot.params;
     this.id = params['id'];
+    this.category_id = 1;
+
+  }
+
+  buscartareas(lista_tareas : any , id : number) {
+    let lista_filtrada : any = [];
+    lista_tareas.forEach((element: any) => {
+      if (element.tablero_id === id) {
+        lista_filtrada.push(element);
+      }
+    });
+    return lista_filtrada;
+  }
+
+  filtrartareascategoria(tareas : any) {
+    let lista_tareas : any = [];
+    let lista_hacer : any = [];
+    let lista_progreso : any = [];
+    let lista_terminado : any = [];
+    if (tareas.length > 0) {
+      tareas.forEach((element:any) => {
+        switch(element.categoria_id) {
+          case 1 : 
+            lista_tareas.push(element);
+          break;
+          case 2 :
+            lista_hacer.push(element);
+          break;
+          case 3 :
+            lista_progreso.push(element);
+          break;
+          case 4 :
+            lista_terminado.push(element);
+          break;
+          default :
+            lista_tareas.push(element);
+          break;
+        }
+      });
+
+      this.lista_tareas = lista_tareas;
+      this.lista_hacer = lista_hacer;
+      this.lista_progreso = lista_progreso;
+      this.lista_terminado = lista_terminado;
 
 
+
+    }
   }
 
 
@@ -57,8 +109,12 @@ export class TasksComponent implements OnInit,AfterViewInit {
 
       let tareasGuardado: any = localStorage.getItem('tareas');
       let tareas = JSON.parse(tareasGuardado);
-      let tarea_filtrada = tareas.find((tarea : any) => tarea.tablero_id === this.id);
-      console.log(" Tarea Filtrada " , tarea_filtrada);
+      let tarea_filtrada = this.buscartareas(tareas,this.id);
+      if (tarea_filtrada.length !== 0) {
+        console.log("El array no está vacío");
+        console.log(" Tarea Filtrada " , tarea_filtrada);
+        this.filtrartareascategoria(tarea_filtrada);
+      }
       // if(tarea_filtrada){
       //   console.log(" Tarea Filtrada " , tarea_filtrada)
       //  // this.formulario_lista.patchValue({title: tarea_filtrada.title, subtitle: tarea_filtrada.subtitle });
@@ -94,13 +150,16 @@ export class TasksComponent implements OnInit,AfterViewInit {
 
   openModal(id:number) {
     console.log(" Open Modal ",id);
+    this.category_id = id;
+    $('#modal-lista').modal('show');
+    $('#category_id').val(id);
     //this.itemId = id;
-    this.isModalVisible = true;
-    console.log(" isModalVisible ", this.isModalVisible)
+    //this.isModalVisible = true;
+    //console.log(" isModalVisible ", this.isModalVisible)
   }
 
   closeModal() {
-    this.isModalVisible = false;
+    $('#modal-lista').modal('hide');
   }
 
   onSubmitLista() {
@@ -114,7 +173,7 @@ export class TasksComponent implements OnInit,AfterViewInit {
         let tarea_json = {
           'id' : lista_tareas.length + 1,
           'tablero_id' : this.id,
-          'categoria_id' : 1,
+          'categoria_id' : this.category_id,
           'title': tablero.title,
           'subtitle' : tablero.subtitle,
         }
@@ -127,7 +186,7 @@ export class TasksComponent implements OnInit,AfterViewInit {
         let tarea_json = {
           'id' : id_tasks,
           'tablero_id' : this.id,
-          'categoria_id' : 1,
+          'categoria_id' : this.category_id,
           'title': tablero.title,
           'subtitle' : tablero.subtitle,
         }
@@ -145,31 +204,124 @@ export class TasksComponent implements OnInit,AfterViewInit {
   }
 
   onSubmitHacer() {
+    let id_tasks = 1;
+    let lista_tareas : any = [];
     if (this.formulario_lista.valid) {
       let tablero = this.formulario_lista.value;
+      if (localStorage.getItem('tareas')) {
+        let tareasGuardado: any = localStorage.getItem('tareas');
+        lista_tareas = JSON.parse(tareasGuardado);
+        let tarea_json = {
+          'id' : lista_tareas.length + 1,
+          'tablero_id' : this.id,
+          'categoria_id' : 2,
+          'title': tablero.title,
+          'subtitle' : tablero.subtitle,
+        }
+        lista_tareas.push(tarea_json);
+        localStorage.setItem('tareas', JSON.stringify(lista_tareas));
+        location.reload();
 
-      console.log(" Tablero ", tablero);
+      } else {
 
+        let tarea_json = {
+          'id' : id_tasks,
+          'tablero_id' : this.id,
+          'categoria_id' : 2,
+          'title': tablero.title,
+          'subtitle' : tablero.subtitle,
+        }
+        lista_tareas.push(tarea_json);
+        console.log(" Tablero ", lista_tareas);
+        localStorage.setItem('tareas', JSON.stringify(lista_tareas));
+        location.reload();
+      }
+
+
+    } else {
+      alert(" Por favor introduzca todos los Datos correspondientes ");
     }
 
   }
 
   onSubmitProgreso() {
+    let id_tasks = 1;
+    let lista_tareas : any = [];
     if (this.formulario_lista.valid) {
       let tablero = this.formulario_lista.value;
+      if (localStorage.getItem('tareas')) {
+        let tareasGuardado: any = localStorage.getItem('tareas');
+        lista_tareas = JSON.parse(tareasGuardado);
+        let tarea_json = {
+          'id' : lista_tareas.length + 1,
+          'tablero_id' : this.id,
+          'categoria_id' : 3,
+          'title': tablero.title,
+          'subtitle' : tablero.subtitle,
+        }
+        lista_tareas.push(tarea_json);
+        localStorage.setItem('tareas', JSON.stringify(lista_tareas));
+        location.reload();
 
-      console.log(" Tablero ", tablero);
+      } else {
 
+        let tarea_json = {
+          'id' : id_tasks,
+          'tablero_id' : this.id,
+          'categoria_id' : 3,
+          'title': tablero.title,
+          'subtitle' : tablero.subtitle,
+        }
+        lista_tareas.push(tarea_json);
+        console.log(" Tablero ", lista_tareas);
+        localStorage.setItem('tareas', JSON.stringify(lista_tareas));
+        location.reload();
+      }
+
+
+    } else {
+      alert(" Por favor introduzca todos los Datos correspondientes ");
     }
 
   }
 
   onSubmitTermiando() {
+    let id_tasks = 1;
+    let lista_tareas : any = [];
     if (this.formulario_lista.valid) {
       let tablero = this.formulario_lista.value;
+      if (localStorage.getItem('tareas')) {
+        let tareasGuardado: any = localStorage.getItem('tareas');
+        lista_tareas = JSON.parse(tareasGuardado);
+        let tarea_json = {
+          'id' : lista_tareas.length + 1,
+          'tablero_id' : this.id,
+          'categoria_id' : 4,
+          'title': tablero.title,
+          'subtitle' : tablero.subtitle,
+        }
+        lista_tareas.push(tarea_json);
+        localStorage.setItem('tareas', JSON.stringify(lista_tareas));
+        location.reload();
 
-      console.log(" Tablero ", tablero);
+      } else {
 
+        let tarea_json = {
+          'id' : id_tasks,
+          'tablero_id' : this.id,
+          'categoria_id' : 4,
+          'title': tablero.title,
+          'subtitle' : tablero.subtitle,
+        }
+        lista_tareas.push(tarea_json);
+        console.log(" Tablero ", lista_tareas);
+        localStorage.setItem('tareas', JSON.stringify(lista_tareas));
+        location.reload();
+      }
+
+
+    } else {
+      alert(" Por favor introduzca todos los Datos correspondientes ");
     }
 
   }
